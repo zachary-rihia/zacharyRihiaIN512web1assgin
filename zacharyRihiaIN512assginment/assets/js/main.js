3652bcc9a212bb9567a703c91cba9a362f887e13
zachary-rihia <rihiz1@student.op.ac.nz>
2021-09-02 15:58:35 +1200

    navbar and hero section changes

--- zacharyRihiaIN512assginment/assets/js/main.js
+++ zacharyRihiaIN512assginment/assets/js/main.js
@@ -1,4 +1,9 @@
 window.addEventListener('load', event => {
 
-    
+    document.querySelector("#mobile-cta").addEventListener("click", () => {
+        document.querySelector(".menu").style.top = 0;
+        document.querySelector(".menu ul").style.opacity = 1;
+        document.querySelector(".menu").style.borderTopRightRadius = 0;
+        document.querySelector(".menu").style.borderTopLeftRadius = 0;
+    })
 });
