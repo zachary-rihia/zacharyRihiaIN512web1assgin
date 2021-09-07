7461f3174c2e387aaea844e2a17c7886d9ea6a36
zachary-rihia <rihiz1@student.op.ac.nz>
2021-09-07 15:01:52 +1200

    recent changes

--- zacharyRihiaIN512assginment/assets/js/main.js
+++ zacharyRihiaIN512assginment/assets/js/main.js
@@ -6,4 +6,14 @@ window.addEventListener('load', event => {
         document.querySelector(".menu").style.borderTopRightRadius = 0;
         document.querySelector(".menu").style.borderTopLeftRadius = 0;
     })
+
+    document.querySelector("#mobile-exit").addEventListener("click", () => {
+        document.querySelector(".menu").style.top = 100;
+        document.querySelector(".menu ul").style.opacity = 0;
+        //document.querySelector(".menu").style.borderTopRightRadius = 0;
+        //document.querySelector(".menu").style.borderTopLeftRadius = 0;
+    })
+
+    
+
 });
