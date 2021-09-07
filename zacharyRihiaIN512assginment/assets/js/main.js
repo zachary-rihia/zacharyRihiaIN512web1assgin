db63c960febf6ccc2658efa50d2b55b9a2bba4fe
zachary-rihia <rihiz1@student.op.ac.nz>
2021-09-07 15:24:12 +1200

    tried making the images appear

--- zacharyRihiaIN512assginment/assets/js/main.js
+++ zacharyRihiaIN512assginment/assets/js/main.js
@@ -8,12 +8,13 @@ window.addEventListener('load', event => {
     })
 
     document.querySelector("#mobile-exit").addEventListener("click", () => {
-        document.querySelector(".menu").style.top = 100;
+        document.querySelector(".menu").style.top = "100%";
         document.querySelector(".menu ul").style.opacity = 0;
-        //document.querySelector(".menu").style.borderTopRightRadius = 0;
-        //document.querySelector(".menu").style.borderTopLeftRadius = 0;
     })
 
-    
+    document.querySelector(".sec-2-img").addEventListener("scroll",() => {
+        document.querySelector(".sec-2-img").style.visibility = "visible";
+        document.querySelector(".sec-2-img").style.opacity = 1;
+    })
 
 });
