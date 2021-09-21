2279df1e084aa1518d80b6a08706088208f5c0a6
zachary-rihia <rihiz1@student.op.ac.nz>
2021-09-21 14:15:32 +1200

    layouts

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+const expressLayouts = require('express-ejs-layouts')
 const path = require("path")
 const port = 3000
 
 app.set("view engine","ejs")
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname,'assets')))
+app.use(expressLayouts)
 
 app.get('/', (req, res) => res.render('index'))
 app.get('/about', (req, res) => res.send('About Page'))
