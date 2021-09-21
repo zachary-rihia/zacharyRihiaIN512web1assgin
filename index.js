da3e4845a9a7b64db0886b07df29576054de64c2
zachary-rihia <rihiz1@student.op.ac.nz>
2021-09-21 14:01:11 +1200

    template update

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require("path")
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set("view engine","ejs")
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname,'assets')))
+
+app.get('/', (req, res) => res.render('index'))
 app.get('/about', (req, res) => res.send('About Page'))
 app.get('/user', (req, res) => res.send('User Page'))
 
