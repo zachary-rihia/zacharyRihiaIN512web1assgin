b425203212a1feb0756468834bd6e3c86df775ea
zachary-rihia <rihiz1@student.op.ac.nz>
2021-09-28 13:22:42 +1300

    added heroku things

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const path = require("path")
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 app.set("view engine","ejs")
 app.set('views', path.join(__dirname, 'views'))
