8dd4a78d4cd4914fc459f3cf48a96a93a1d6b5c0
zachary-rihia <rihiz1@student.op.ac.nz>
2021-09-30 11:49:12 +1300

    added comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 
 window.addEventListener('load', event => {
 
+    //mobile menu system the comes up from the bottom of the screen
     document.querySelector("#mobile-cta").addEventListener("click", () => {
         document.querySelector(".menu").style.top = 0;
         document.querySelector(".menu ul").style.opacity = 1;
@@ -8,6 +9,7 @@ window.addEventListener('load', event => {
         document.querySelector(".menu").style.borderTopLeftRadius = 0;
     })
 
+    //the mobile exit button that makes the menu go back from where it came
     document.querySelector("#mobile-exit").addEventListener("click", () => {
         document.querySelector(".menu").style.top = "100%";
         document.querySelector(".menu ul").style.opacity = 0;
@@ -16,6 +18,7 @@ window.addEventListener('load', event => {
     let lastScrollPosition = 0;
     let ticking = false;
 
+    //selects all the imgs of the 2nd section to make them do all the same animation
     function callimg2(scrollPos){
 
         document.querySelectorAll(".sec-2-img").forEach(img => {
@@ -26,6 +29,7 @@ window.addEventListener('load', event => {
         
     }
 
+    //scrolling event which makes the pictures appear on the 2nd section cutting off some of the words in the quote
     document.addEventListener("scroll", function(e){
         lastScrollPosition = window.screenY;
 
@@ -41,6 +45,7 @@ window.addEventListener('load', event => {
     
     let currleft = 0;
 
+    //flicks through the image gallery via the next button
     document.querySelector(".charas-arrow-right").addEventListener("click", () => {
         let img = document.querySelector(".sec-3-img img:first-of-type");
         console.log(img.getBoundingClientRect());
@@ -48,6 +53,7 @@ window.addEventListener('load', event => {
         img.style.marginLeft = currleft + "%";
     })
 
+    //flicks through the image gallery via the prev button
     document.querySelector(".charas-arrow-left").addEventListener("click", () => {
         let img = document.querySelector(".sec-3-img img:first-of-type");
         console.log(img.getBoundingClientRect());
