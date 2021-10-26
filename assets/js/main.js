e43023cadb31d99f633b42943d6fc1e2d995da12
zachary-rihia <rihiz1@student.op.ac.nz>
2021-10-26 14:57:39 +1300

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,10 @@ window.addEventListener('load', event => {
     document.querySelector("#mobile-cta").addEventListener("click", () => {
         document.querySelector(".menu").style.top = 0;
         document.querySelector(".menu ul").style.opacity = 1;
+        document.querySelector(".contacts").style.opacity = 1;
         document.querySelector(".menu").style.borderTopRightRadius = 0;
         document.querySelector(".menu").style.borderTopLeftRadius = 0;
+        document.querySelector(".footer-nav-bar").style.zIndex = 4;
     })
 
     //the mobile exit button that makes the menu go back from where it came
